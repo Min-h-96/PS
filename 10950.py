43e56d82755edead4915ec1db72349d1ed2c647c
Min-h-96 <asdf6256@gmail.com>
2020-09-10 03:52:53 +0900

    2020.09.10

---
+++ 10950.py
@@ -0,0 +1,5 @@
+# 백준 10950번 문제.
+T = int(input())
+for i in range(T):
+    a, b = map(int, input().split())
+    print(a + b)
---
+++ 15552.py
@@ -0,0 +1,5 @@
+# 백준 15552번 문제. 빠른 A+B
+import sys
+
+T = sys.stdin.readline()
+a, b = map(int, sys.stdin.readline().split())
