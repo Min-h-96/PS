c4e72fd9bc1734d785d595a22b631e23c667701f
Min-h-96 <asdf6256@gmail.com>
2020-09-22 13:11:46 +0900

    2020.09.22

--- 8단계-수학1/2839.py
+++ 8단계-수학1/2839.py
@@ -14,3 +14,21 @@ def sugar(N):
 N = int(sys.stdin.readline())
 
 print(sugar(N))
+
+"""
+n = int(input())
+F = n//5
+n%=5
+T = 0
+while F>=0:
+    if n%3 == 0:
+        T = n//3
+        n = n%3
+        break
+    F-=1
+    n+=5
+print((n==0) and (F+T) or -1)
+
+
+출처: https://doorbw.tistory.com/60 [Tigercow.Door]
+"""
