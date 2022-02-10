9f030c57f3048ca347fe721faadb4d8699bfc114
Min-h-96 <asdf6256@gmail.com>
2022-02-10 11:07:49 +0900

    22.2.10. PS

---
+++ Programmers/Level_3/3_35.cpp
@@ -0,0 +1,41 @@
+/* **************************************************************
+    모두 0으로 만들기
+************************************************************** */
+
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+vector<int> parent;
+
+bool cmp(vector<int>& a, vector<int>& b) {
+    return a[2] < b[2];
+}
+
+int findParent(int A) {
+    if (A == parent[A]) return A;
+    return parent[A] = findParent(parent[A]);
+}
+
+int solution(int n, vector<vector<int>> costs) {
+    int answer = 0;
+    
+    for (auto i=0; i<n; i++)
+        parent.push_back(i);
+    
+    sort(costs.begin(), costs.end(), cmp);
+    
+    for (const auto& c : costs) {
+        int node1 = c[0];
+        int node2 = c[1];
+        int cost = c[2];
+        if (findParent(node1) != findParent(node2)) {
+            parent[findParent(node2)] = findParent(node1);
+            answer += cost;
+        }
+    }
+    
+    return answer;
+}
