4b0d6644d1f7a3fed09f069ea25a26f7b9b2f461
Min-h-96 <asdf6256@gmail.com>
2024-03-05 23:53:43 +0900

    240305 PS

--- 240305_BOJ2667/240305_BOJ2667.py
+++ 240305_BOJ2667/240305_BOJ2667.py
@@ -4,13 +4,8 @@ import sys
 from collections import deque
 
 N = int(sys.stdin.readline())
-board = [list(sys.stdin.readline().strip()) for _ in range(N)]
+board = [list(map(int, sys.stdin.readline().strip())) for _ in range(N)]
 answers = []
-checked = [[False for _ in range(N)] for _ in range(N)]
-for i in range(N):
-	for j in range(N):
-		if (board[i][j] == '0'):
-			checked[i][j] = True
 
 dq = deque()
 dy = [1, -1, 0, 0]
@@ -18,26 +13,26 @@ dx = [0, 0, 1, -1]
 
 for i in range(N):
 	for j in range(N):
-		if (checked[i][j]):
+		if (board[i][j] == 0):
 			continue
-			
+		
 		dq.append((i, j))
-		checked[i][j] = True
+		board[i][j] = 0
 		cnt = 1
 		while (len(dq)):
 			cy, cx = dq.popleft()
 
-			for i in range(4):
-				ny = cy + dy[i]
-				nx = cx + dx[i]
+			for k in range(4):
+				ny = cy + dy[k]
+				nx = cx + dx[k]
 
-				if (ny < 0 or ny >= N or nx < 0 or nx >= N or checked[ny][nx]):
+				if (ny < 0 or ny >= N or nx < 0 or nx >= N or board[ny][nx] == 0):
 					continue
 				
 				dq.append((ny, nx))
-				checked[ny][nx] = True
+				board[ny][nx] = 0
 				cnt += 1
-
+		
 		answers.append(cnt)
 
 answers.sort()
